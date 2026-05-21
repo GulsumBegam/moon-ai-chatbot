a8a0c72e116ee3cb63ec1d39442fe7d874039f05
gulsumbegam1 <gulsum2004begam@gmail.com>
2026-05-21 13:59:27 +0530

    disable type checking

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   serverExternalPackages: ['@prisma/client'],
 
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     remotePatterns: [
       {
