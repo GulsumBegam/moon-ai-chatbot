f64dc2dd0e485c4968a48dfd230367ae1e40f679
gulsumbegam1 <gulsum2004begam@gmail.com>
2026-05-21 12:56:42 +0530

    fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
+  serverExternalPackages: ['@prisma/client'],
+
   images: {
     remotePatterns: [
       {
